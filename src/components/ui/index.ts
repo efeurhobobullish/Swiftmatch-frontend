d9d0318b0a9e8a4c527defda71e3ac38ded197f9
Empire Tech <151479661+efeurhobobullish@users.noreply.github.com>
2025-11-24 09:50:05 +0100

    Update index.ts

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -4,6 +4,7 @@ export {default as InputWithoutIcon} from "./InputWithoutIcon";
 export {default as SelectWithIcon} from "./SelectWithIcon";
 export {default as SelectWithoutIcon} from "./SelectWithoutIcon";
 export {default as ScrollToTop} from "./ScrollToTop";
+export {default as ModeToggle} from "./mode-toggle";
 export {default as GobackButton} from "./GobackButton";
 export {default as Pattern} from "./pattern";
 

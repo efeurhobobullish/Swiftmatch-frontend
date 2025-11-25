7a6f3076977aa08e8a8bae80291f872af1619e33
Empire Tech <151479661+efeurhobobullish@users.noreply.github.com>
2025-11-25 04:10:35 +0100

    Create useAuth.ts

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,10 @@
+export const useAuth = () => {
+  return {
+    user: {
+      name: "User882",
+      email: "user@example.com",
+      phone: "08123456789",
+      wallet: 12500.55 // Demo balance
+    }
+  };
+};

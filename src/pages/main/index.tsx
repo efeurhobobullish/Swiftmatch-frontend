adc9c6052c0061d14d500a86a31a14c38c42ceba
Empire Tech <151479661+efeurhobobullish@users.noreply.github.com>
2025-11-25 03:37:37 +0100

    Update index.tsx

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,1 +1,6 @@
 export { default as Dashboard } from "./dashboard";
+export { default as History } from "./History";
+export { default as Services } from "./Services";
+export { default as Wallet } from "./Wallet";
+export { default as Profile } from "./Profile";
+export { default as Support } from "./Support";
